42aba90c06ed85ca081a312f5e773f2a76df829b
Evelien Hermans <hermansevelien@hotmail.com>
2024-04-26 13:36:44 +0200

    basic Flask code - aanpassing tekst

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
+app=Flask(__name__)
+
 @app.route ('/')
 def home () :
     return "<p>Website met vallen en opstaan.</p>"
 
-
-app=Flask(__name__)
 if __name__ == '__main__':
     app.run (port=5000,debug=True)
